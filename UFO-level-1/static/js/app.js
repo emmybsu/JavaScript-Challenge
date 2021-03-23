f58c5a57922a7d58ff3e92e837f6c6b8903532dc
emmybsu <74212834+emmybsu@users.noreply.github.com>
2021-03-23 18:44:57 -0400

    code review

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -2,7 +2,7 @@
 var sightings = data;
 
 // select the button
-var button = d3.select('#filter-btn');
+const button = d3.select('#filter-btn');
 // select the form 
 var form = d3.select('#filters');
 
@@ -10,6 +10,18 @@ var form = d3.select('#filters');
 button.on('click', runEnter);
 form.on('submit', runEnter);
 
+
+function createTable(filteredData) {
+  var table = document.getElementById('ufo-table')
+  tbody.html('');   
+  filteredData.forEach((dataRow)=>{ 
+    var row = tbody.append('tr');
+    Object.values(dataRow).forEach((val)=> {
+      let cell = row.append('td');
+      cell.text(val);
+    })
+  })
+};
 // complete the event handler function for the form
 function runEnter() {
 
@@ -26,37 +38,35 @@ function runEnter() {
 //   console.log(people);
   var filteredData = sightings.filter(ufoSightings => ufoSightings.datetime === inputValue);
 
+  if (inputValue){filteredData = filteredData.filter(ufoSightings => ufoSightings.datetime === inputValue);}
+
   console.log(filteredData)
-
-
-
+ 
 //referenced from https://www.youtube.com/watch?v=XmdOZ5NSqb8
-createTable(filteredData)
+createTable(filteredData)};
 
-function createTable(filteredData) {
-    var table = document.getElementById('ufo-table')
 
-    for (var i = 0; i < filteredData.length; i++ ){
-        var row = `<tr>
-                       <td> ${filteredData[i].datetime}</td> 
-                       <td> ${filteredData[i].city}</td> 
-                       <td> ${filteredData[i].state}</td> 
-                       <td> ${filteredData[i].country}</td> 
-                       <td> ${filteredData[i].shape}</td> 
-                       <td> ${filteredData[i].durationMinutes}</td> 
-                       <td> ${filteredData[i].comments}</td> 
+
+    // for (var i = 0; i < filteredData.length; i++ ){
+    //     var row = `<tr>
+    //                    <td> ${filteredData[i].datetime}</td> 
+    //                    <td> ${filteredData[i].city}</td> 
+    //                    <td> ${filteredData[i].state}</td> 
+    //                    <td> ${filteredData[i].country}</td> 
+    //                    <td> ${filteredData[i].shape}</td> 
+    //                    <td> ${filteredData[i].durationMinutes}</td> 
+    //                    <td> ${filteredData[i].comments}</td> 
           
-                   </tr>`
-        table.innerHTML += row
+    //                </tr>`
+    //     table.innerHTML += row
+
+        
+        // var list = d3.select('tbody');
+        // list.html('');
 
      //}
   // Then, select the unordered list element by class name
   //var list = d3.select(".table table-striped");
 
-    }
-    var list = d3.select('.table table-striped');
+    //};
 
-    list.html(' ');
-    
-    }
-};
